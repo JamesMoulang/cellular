edfa2a848663ac63663f5616661a6340672e1f07
James Moulang <jamesmoulang123@gmail.com>
2016-06-11 16:26:45 +0100

    drum pair

--- app/entities/DrumPair.js
+++ app/entities/DrumPair.js
@@ -5,10 +5,19 @@ class DrumPair extends Destroyable {
 	constructor(game, leftkey, rightkey, position, leftnotes, rightnotes, play) {
 		this.position = position;
 		this.leftdrum = new Drum(this.game, leftkey, position, leftnotes);
-		this.rightdrum = new Drum(this.game, leftkey, position, leftnotes);
+		this.rightdrum = new Drum(this.game, rightkey, position, rightnotes);
 	}
 
 	tick() {
+		this.leftdrum.tick();
+		this.rightdrum.tick();
+	}
+
+	update() {
+
+	}
+
+	render() {
 
 	}
 }
